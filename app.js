ac41c6da3f71c581d641e7d56c61e8806e8ee1e5
Cencio Peravolos <cencioperavolos@yahoo.it>
2020-04-02 17:23:52 +0200

    Added Mongo db Atlas

--- app.js
+++ app.js
@@ -10,15 +10,22 @@ const express = require("express"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
     User = require("./models/user")
-    seedDb = require("./seeds");
+seedDb = require("./seeds");
 
 // requiring routes
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect('mongodb://localhost/yelp_camp',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+// mongodb+srv://testUser:<password>@cluster0-qnesc.mongodb.net/test?retryWrites=true&w=majority
+
+// mongoose.connect('mongodb://localhost/yelp_camp',
+mongoose.connect('mongodb+srv://testUser:testPassuord@cluster0-qnesc.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+        console.log('Connected to DB.')
+    }).catch((err) => {
+        console.log("Problems on connecting to DB", err);
+    });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -40,7 +47,7 @@ passport.deserializeUser(User.deserializeUser());
 
 //POST PASSPORT CONFIGURATION
 // Add all responses varaible currentUser
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash(("error"));
     res.locals.success = req.flash(("success"));
@@ -49,9 +56,9 @@ app.use(function(req, res, next){
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
-app.use( "/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-var port = process.env.PORT || 80
+var port = process.env.PORT || 3000
 app.listen(port, function () {
     console.log("YelpCamp express server listening on port 80!");
 }) 
